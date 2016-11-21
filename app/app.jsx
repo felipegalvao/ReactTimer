2aa2a48efceb431ecff6965d38f659fc2db7bd08
Felipe Galvao <contato@felipegalvao.com.br>
2016-11-21 10:54:32 -0200

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+// Load app.css
+require('style!css!sass!applicationStyles');
+$(document).foundation();
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>      
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer</li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active-link">Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Feito por <a href="http://www.felipegalvao.com.br" target="_blank">Felipe Galvão</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );    
+  }
+})
+
+module.exports = Nav;
