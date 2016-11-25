0480af83290e8ae8fc514af5f53b2e3333567704
Felipe Galvao <contato@felipegalvao.com.br>
2016-11-25 15:48:57 -0200

    Finish countdown app

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -68,6 +68,7 @@ var Countdown = React.createClass({
 
     return (
       <div>
+        <h1 className="page-title">Countdown App</h1>
         <Clock totalSeconds={count}/>
         {renderControlArea()}
       </div>
